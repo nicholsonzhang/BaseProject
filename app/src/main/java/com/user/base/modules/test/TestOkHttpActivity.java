b5dc09b4e8f1e09df72dcb7da29f3f68278e4321
nichols <zhang.e.ong@gmail.com>
2018-05-16 22:25:41 +0800

    test ok http

---
+++ app/src/main/java/com/user/base/modules/test/TestOkHttpActivity.java
@@ -0,0 +1,19 @@
+package com.user.base.modules.test;
+
+import com.user.base.AppBaseActivity;
+import com.user.base.R;
+
+
+public class TestOkHttpActivity extends AppBaseActivity{
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_test_ok_http;
+    }
+
+    @Override
+    protected void initViews() {
+
+
+
+    }
+}
---
+++ test/src/main/java/com/and/framework/test/TestInterval.java
@@ -0,0 +1,43 @@
+package com.and.framework.test;
+
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class TestInterval {
+
+
+    public static void main(String args[]) {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+
+            }
+        }).subscribeOn(Schedulers.io())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+}
