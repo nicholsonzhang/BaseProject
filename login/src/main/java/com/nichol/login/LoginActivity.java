3c103e9918e973cb1fc0eba656ac3896fee1054c
张业勇 <yeyong.zhang@phicomm.com>
2018-08-07 20:10:18 +0800

    add more file

---
+++ login/src/main/java/com/nichol/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.nichol.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_login);
+    }
+}
